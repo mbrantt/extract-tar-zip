76f8a24228012ccf675908db50e56b28509f0727
mbrantt <mbrantt@nobelti.cl>
2021-07-22 16:47:50 -0400

    esquema basico

---
+++ src/main/java/mbrantt/nobelti/app/App.java
@@ -0,0 +1,14 @@
+package mbrantt.nobelti.app;
+
+import java.util.zip.*;
+
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
